202f87dc53d6fc261c6adb646e19462c23c9d555
SENGHAK.CHHUN <SENGHAK.CHHUN@PNC.LAN>
2023-11-18 15:10:09 +0700

    18 november 2023

--- G-S/pr/main.js
+++ G-S/pr/main.js
@@ -1,28 +1,17 @@
-const li = document.querySelector('#Teacher');
-const container = li.parentElement.parentElement;
-console.log(li.parentElement)
-console.log(li.previousElementSibling.previousElementSibling.previousElementSibling)
-console.log(li.parentElement.parentElement)
-console.log(li.parentElement.nextElementSibling)
-
-let div = document.createElement('div');
+let li = document.getElementById('Teacher');
+// console.log(li.parentElement)
+// let li4 = li.previousElementSibling.previousElementSibling
+// console.log(li4)
+let container = document.querySelector('.container');
+let div = document.createElement('div')
+div.className = 'group'
 let h1 = document.createElement('h1');
-h1.textContent = 'Go home in Khmer new year day'
+h1.textContent = 'Hollo group study'
 div.appendChild(h1)
 container.appendChild(div)
-let btn = document.createElement('button');
-btn.textContent  = 'choose me';
-
-let btn1 = document.createElement('button');
-btn1.textContent  = 'choose ke';
-
-let btn2 = document.createElement('button');
-btn2.textContent  = 'choose yourself';
-
-btn.setAttribute('id','btn');
-btn1.className = 'btn';
-btn2.classList.add('btn')
-container.appendChild(btn)
-container.appendChild(btn1)
-container.appendChild(btn2)
-console.log(container)
+// parentElement
+// children
+// firstElementChild
+// lastElementChild
+// previousElementSibling
+// nextElementSibling
